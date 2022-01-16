b5a6849beba706a64eb9c51234efcda2760970c7
wrmn <aditya_wrmn@protonmail.com>
2022-01-16 13:03:27 +0700

    minor pacs.002 fix

--- iso20022/pacs/FIToFIPaymentStatusReportV10.go
+++ iso20022/pacs/FIToFIPaymentStatusReportV10.go
@@ -8,7 +8,7 @@ import (
 
 type Document00200110 struct {
 	XMLName xml.Name                      `xml:"urn:iso:std:iso:20022:tech:xsd:pacs.002.001.10 Document" json:"-"`
-	Message *FIToFIPaymentStatusReportV10 `xml:"FIToFIPmtStsRpt" json:"FIToFIPmtStsRpt"`
+	Message *FIToFIPaymentStatusReportV10 `xml:"FIToFIPmtStsRpt" json:"FItoFIPmtStsRpt"`
 }
 
 func (d *Document00200110) AddMessage() *FIToFIPaymentStatusReportV10 {
